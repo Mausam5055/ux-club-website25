389b05c3d803489baa417b43cd4550935e4de856
Raj kumar <186265760+Rajkr76@users.noreply.github.com>
2026-02-07 04:42:32 +0530

    fixingg3

--- app/Blog/components/BlogCard.jsx
+++ app/Blog/components/BlogCard.jsx
@@ -1,8 +1,6 @@
 "use client";
 
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 import { blogData } from './blogData';
 
